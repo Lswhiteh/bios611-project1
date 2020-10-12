b107d30b485373c7bee36a06209698245f28fde0
Logan Whitehouse <lswhiteh@live.unc.edu>
2020-10-12 16:27:03 -0400

    Updated feature plot to show legend on bottom. Might remove?

--- plot_edible_v_inedible_chars.R
+++ plot_edible_v_inedible_chars.R
@@ -17,9 +17,11 @@ ggplot(unique_vals, aes(x=L1, y=value, fill=L1)) +
   geom_bar(stat="identity") +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
-        axis.ticks.x=element_blank()) +
+        axis.ticks.x=element_blank(),
+        legend.position="bottom") +
   scale_fill_discrete(name="Features") +
   labs(y="Possible Categories", x=NULL) +
   scale_y_continuous(breaks=0:15) +
+  guides(fill = guide_legend(label.position = "bottom")) +
   ggtitle("Number of Options for Categorical Features")
 ggsave('figures/category_options.png')
